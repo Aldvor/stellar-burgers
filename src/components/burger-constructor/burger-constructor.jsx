30773e1286a317890409f34fed941a383ad4dadf
Dmitriy Loktionov <tanarica@yandex.ru>
2023-09-10 14:15:28 +0300

    first version project

---
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -0,0 +1,47 @@
+import {ConstructorElement, CurrencyIcon, DragIcon, Button} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from "./burger-constructor.module.css";
+import PropTypes from 'prop-types';
+import ingredientPropType from '../../utils/prop-types';
+
+const BurgerConstructor = ({bun, ingredients}) => {
+
+    const totalPrice = ingredients.reduce((acc, ingredient) => acc + ingredient.price, 0) + (bun ? bun.price * 2 : 0);
+
+    return (
+        <section className={`${styles.section} mt-15`}>
+            <ul className={styles['outer-list']}>
+                {bun && <li className={styles.bun}><ConstructorElement type="top" isLocked={true} text={`${bun.name} (верх)`} price={bun.price} thumbnail={bun.image}/></li>}
+                <li>
+                    <ul className={`${styles['inner-list']} custom-scroll`}>
+                        {ingredients.map((ingredient) => (
+                            <li key={ingredient._id} className={styles.item}>
+                                <DragIcon type="primary"/>
+                                <ConstructorElement text={ingredient.name} price={ingredient.price} thumbnail={ingredient.image} />
+                            </li>
+                        ))}
+                    </ul>
+                </li>
+
+                {bun &&
+                    <li className={styles.bun}><ConstructorElement type="bottom" isLocked={true} text={`${bun.name} (низ)`} price={bun.price} thumbnail={bun.image}/></li>}
+            </ul>
+
+            <div className={`${styles.checkout} mr-4 mt-10`}>
+                <div className={styles.total}>
+                    <span className="text text_type_digits-medium">{totalPrice}</span>
+                    <CurrencyIcon type="primary"/>
+                </div>
+                <Button htmlType="button" type="primary" size="large">
+                    Оформить заказ
+                </Button>
+            </div>
+        </section>
+    );
+};
+
+BurgerConstructor.propTypes = {
+    bun: ingredientPropType,
+    ingredients: PropTypes.arrayOf(ingredientPropType).isRequired,
+};
+
+export default BurgerConstructor;
---
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useRef } from 'react';
+import styles from "./burger-ingredients.module.css";
+import {Tab, CurrencyIcon, Counter} from '@ya.praktikum/react-developer-burger-ui-components';
+import PropTypes from 'prop-types';
+import ingredientPropType from '../../utils/prop-types';
+
+const BurgerIngredients = ({data, addIngredientToCart}) => {
+
+    const [current, setCurrent] = useState('Булки');
+
+    const categorizedItems = {
+        'Булки': data.filter(item => item.type === 'bun'),
+        'Соусы': data.filter(item => item.type === 'sauce'),
+        'Начинки': data.filter(item => item.type === 'main'),
+    };
+
+    const refs = {
+        'Булки': React.useRef(null),
+        'Соусы': React.useRef(null),
+        'Начинки': React.useRef(null),
+    };
+
+    const handleTabClick = (tab) => {
+        setCurrent(tab);
+        refs[tab].current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    return (
+        <section className={styles.section}>
+            <h1 className="text text_type_main-large mb-5">Соберите свой бургер</h1>
+            <div className={styles.tabs}>
+                {Object.keys(categorizedItems).map(category => (
+                    <Tab key={category} value={category} active={current === category} onClick={() => handleTabClick(category)}>
+                        {category}
+                    </Tab>
+                ))}
+            </div>
+
+            <ul className={`${styles['create-burger']} custom-scroll`}>
+                {Object.entries(categorizedItems).map(([category, ingredients]) => (
+                    <li key={category} ref={refs[category]}>
+                        <h2 className="text text_type_main-medium mt-10">{category}</h2>
+                        <ul className={`${styles.ingredients} mt-6 pl-4`}>
+                            {ingredients.map(({_id, type, name, price, image}) => (
+                                <li
+                                    key={_id}
+                                    className={styles.ingredient}
+                                    onClick={() => addIngredientToCart({_id, type, name, price, image})}
+                                >
+                                    <Counter count={1} size="default" extraClass="m-1" />
+                                    <img className='pl-4 pr-4' src={image} alt={name} width="240" height="120"/>
+                                    <div className={styles.price}>
+                                        <span className="text text_type_digits-default">{price}</span>
+                                        <CurrencyIcon type="primary"/>
+                                    </div>
+                                    <span className="text text_type_main-default">{name}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
+
+BurgerIngredients.propTypes = {
+    data: PropTypes.arrayOf(ingredientPropType).isRequired,
+    addIngredientToCart: PropTypes.func.isRequired,
+};
+
+export default BurgerIngredients;
