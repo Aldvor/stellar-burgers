9b8803acbd073f07e6a704786fdc10333addc6e8
Dmitriy Loktionov <tanarica@yandex.ru>
2023-09-17 00:52:06 +0300

    react api

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,4 +1,4 @@
-import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import {Logo, BurgerIcon, ListIcon, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./app-header.module.css";
 import React from "react";
 
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,36 +1,45 @@
-import React, { useState, useRef } from 'react';
+import React, {useState, useRef} from "react";
 import styles from "./burger-ingredients.module.css";
-import {Tab, CurrencyIcon, Counter} from '@ya.praktikum/react-developer-burger-ui-components';
-import PropTypes from 'prop-types';
-import ingredientPropType from '../../utils/prop-types';
+import {Tab, CurrencyIcon, Counter} from "@ya.praktikum/react-developer-burger-ui-components";
+import PropTypes from "prop-types";
+import ingredientPropType from "../../utils/prop-types";
 
-const BurgerIngredients = ({data, addIngredientToCart}) => {
+const BurgerIngredients = React.memo(({items, addIngredientToCart, isLoading, error, openModal}) => {
 
     const [current, setCurrent] = useState('Булки');
 
-    const categorizedItems = {
-        'Булки': data.filter(item => item.type === 'bun'),
-        'Соусы': data.filter(item => item.type === 'sauce'),
-        'Начинки': data.filter(item => item.type === 'main'),
-    };
+    const categorizedItems = React.useMemo(() => ({
+        'Булки': items.filter(item => item.type === 'bun'),
+        'Соусы': items.filter(item => item.type === 'sauce'),
+        'Начинки': items.filter(item => item.type === 'main'),
+    }), [items]);
 
     const refs = {
-        'Булки': React.useRef(null),
-        'Соусы': React.useRef(null),
-        'Начинки': React.useRef(null),
+        'Булки': useRef(null),
+        'Соусы': useRef(null),
+        'Начинки': useRef(null),
     };
 
     const handleTabClick = (tab) => {
         setCurrent(tab);
-        refs[tab].current.scrollIntoView({ behavior: 'smooth' });
+        refs[tab].current.scrollIntoView({behavior: 'smooth'});
     };
+
+    if (isLoading) {
+        return <p>Загрузка...</p>;
+    }
+
+    if (error) {
+        return <p>Произошла ошибка. Пожалуйста, перезагрузите страницу.</p>;
+    }
 
     return (
         <section className={styles.section}>
-            <h1 className="text text_type_main-large mb-5">Соберите свой бургер</h1>
+            <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
             <div className={styles.tabs}>
                 {Object.keys(categorizedItems).map(category => (
-                    <Tab key={category} value={category} active={current === category} onClick={() => handleTabClick(category)}>
+                    <Tab key={category} value={category} active={current === category}
+                         onClick={() => handleTabClick(category)}>
                         {category}
                     </Tab>
                 ))}
@@ -40,20 +49,23 @@ const BurgerIngredients = ({data, addIngredientToCart}) => {
                 {Object.entries(categorizedItems).map(([category, ingredients]) => (
                     <li key={category} ref={refs[category]}>
                         <h2 className="text text_type_main-medium mt-10">{category}</h2>
-                        <ul className={`${styles.ingredients} mt-6 pl-4`}>
-                            {ingredients.map(({_id, type, name, price, image}) => (
+                        <ul className={`${styles.ingredients} mt-6 ml-4`}>
+                            {ingredients.map((ingredient) => (
                                 <li
-                                    key={_id}
+                                    key={ingredient._id}
                                     className={styles.ingredient}
-                                    onClick={() => addIngredientToCart({_id, type, name, price, image})}
+                                    onClick={() => {
+                                        openModal(ingredient);
+                                        addIngredientToCart(ingredient);
+                                    }}
                                 >
-                                    <Counter count={1} size="default" extraClass="m-1" />
-                                    <img className='pl-4 pr-4' src={image} alt={name} width="240" height="120"/>
+                                    <Counter count={1} size="default" extraClass="m-1"/>
+                                    <img src={ingredient.image} alt={ingredient.name} width="240" height="120"/>
                                     <div className={styles.price}>
-                                        <span className="text text_type_digits-default">{price}</span>
+                                        <span className="text text_type_digits-default">{ingredient.price}</span>
                                         <CurrencyIcon type="primary"/>
                                     </div>
-                                    <span className="text text_type_main-default">{name}</span>
+                                    <span className="text text_type_main-default">{ingredient.name}</span>
                                 </li>
                             ))}
                         </ul>
@@ -62,11 +74,14 @@ const BurgerIngredients = ({data, addIngredientToCart}) => {
             </ul>
         </section>
     );
-};
+});
 
 BurgerIngredients.propTypes = {
-    data: PropTypes.arrayOf(ingredientPropType).isRequired,
+    items: PropTypes.arrayOf(ingredientPropType).isRequired,
     addIngredientToCart: PropTypes.func.isRequired,
+    isLoading: PropTypes.bool.isRequired,
+    error: PropTypes.bool.isRequired,
+    openModal:  PropTypes.func.isRequired,
 };
 
 export default BurgerIngredients;
